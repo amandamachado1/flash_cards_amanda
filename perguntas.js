71c5094d5c066c1e00dfe5909e5a3187fcbe11ca
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2024-10-25 08:27:08 -0300

    programaçao

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Meio Ambiente',
+    'O que é Desmatamento?',
+    'É a remoção das árvores de uma área'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual e a capital da Austrália?',
+    'Canberra'
+)
+
+criaCartao(
+    'Meio Ambiente',
+    'O que é Poluição do ar?',
+    'É a contaminação da atmosfera com substâncias tóxicas'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos Continetes existem?',
+    'Seis'
+)
